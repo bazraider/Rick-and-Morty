1d9bfb6423e0a5eecfd42c8e481facc94152596e
Vasiliy <baz_raider@mail.ru>
2022-07-12 00:20:29 +0700

    styles for cards on IndexPage

--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import './IndexPage.scss';
+import characters_img from '../../static/characters.jpg'
+import episodes_img from '../../static/episodes.jpg'
+import location_img from '../../static/location.jpg'
 
 export default function IndexPage() {
   return (
-    <div>IndexPage</div>
+    <div className="chapters__wrapper">
+      <h1>Выберите интересующий раздел</h1>
+      <div className="chapters__cards">
+        <div class="card__outer">
+          <div class="card__inner">
+            <img src={characters_img} alt="img of characters" />
+            <div className="card__inner__text">
+              <h2>Персонажи</h2>
+            </div>
+          </div>
+        </div>
+        <div class="card__outer">
+          <div class="card__inner">
+            <img src={episodes_img} />
+            <div className="card__inner__text">
+              <h2>Эпизоды</h2>
+            </div>
+          </div>
+        </div>
+        <div class="card__outer">
+          <div class="card__inner">
+            <img src={location_img} alt="img of location" />
+            <div className="card__inner__text">
+              <h2>Локации</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
