24a93934a117ec7341c4e6528001d0b63e77a0a2
Vasiliy <baz_raider@mail.ru>
2022-07-13 23:22:45 +0700

    edit person card in PersonPage

--- src/pages/PersonPage/PersonPage.jsx
+++ src/pages/PersonPage/PersonPage.jsx
@@ -2,38 +2,53 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { getApiResource } from '../../utils/network';
 import { BackButton } from '../../components/CharactersPage/BackButton/BackButton'
+import './PersonPage.scss'
 
 export default function PersonPage() {
   const { id } = useParams();
-
   // Заведём стейт для информации про персонажа
   const [personInfo, setPersonInfo] = useState(null);
   // Добавим лоадер
   const [loading, setLoading] = useState(true);
 
-  console.log(personInfo); // =================================================================> TODO удалить
+  console.log(personInfo);
+
   // Получаем конкретного персонажа с API с использованием id из useParams
   useEffect(() => {
-    const getDataFunc = (async () => {
-      const info = await getApiResource(`https://rickandmortyapi.com/api/character/${id}`);
-      const { data } = info;
-      setPersonInfo(data);
-      setLoading(false);
-    })()
-      .catch(console.error);
+    setTimeout(() => {
+      const getDataFunc = (async () => {
+        const info = await getApiResource(`https://rickandmortyapi.com/api/character/${id}`);
+        const { data } = info;
+        setPersonInfo(data);
+        setLoading(false);
+      })()
+        .catch(console.error);
+    }, 1000);
   }, []);
 
-  // отображаем спинер если идёт загрузка
-  return loading
-    ? (<div className="spinner"></div>)
-    : (
-      <>
-        <BackButton />
-        <div className="person__wrapper">
-          <img src={personInfo.image} alt="" />
-          <h2>{personInfo.name}</h2>
-          <h2>{personInfo.species}</h2>
-          <h2>{personInfo.status}</h2>
+  // Отображаем спинер если идёт загрузка
+  return (
+    <>
+      <BackButton />
+      {loading
+        ? (<div className="spinner"></div>)
+        : <div className="card__outer">
+          <div className="card__inner">
+            <img src={personInfo.image} alt="img of characters" />
+            <div className="card__inner__text">
+              <h1>{personInfo.name}</h1>
+              <div className="person__status">
+                <div className="indicator"></div>
+                <h3>{personInfo.status} - {personInfo.species} - {personInfo.gender}</h3>
+              </div>
+              <div className="person__location">
+                <h4>Last known location:</h4>
+                <h3>{personInfo.location.name}</h3>
+              </div>
+            </div>
+          </div>
         </div>
-      </>)
+      }
+    </>
+  )
 }
